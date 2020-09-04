426b0b04b0eea3e7a579a0c2b6104415557e46f9
LanMengCha <3207374583@qq.com>
2020-09-04 19:23:26 +0800

    第一次提交

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <ncurses.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+//const int
+//const int
+//const int
+//unsigned const int
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+//using namespace std;
+int main()
+{
+    return 0;
+}
