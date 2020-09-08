2e54aa7b8e0fe4cbf3052a9e9972f50186967466
LanMengCha <3207374583@qq.com>
2020-09-08 21:44:58 +0800

    9/8

--- function.cpp
+++ function.cpp
@@ -1,2 +1,10 @@
 #include "header.h"
-
+//using namespace std;
+void std_window(void);
+//函数原型（我为什么还要写这玩意）
+void std_window(void)
+{
+    initscr();
+    raw();
+}
+//窗口生成函数（构造？）
--- player.cpp
+++ player.cpp
@@ -1,2 +1,23 @@
 #include "header.h"
 //玩家类
+class player
+{
+    public:
+	player(int hp,unsigned int atk);//构造函数
+	~player();
+	
+    private:
+        int health;
+	unsigned int attack;
+}
+//类函数定义：
+player::player(int hp,unsigned int atk)
+{
+    health = hp;
+    attack = atk;
+}//构造函数函数体
+player::~player()
+{
+    printw("GAME_OVER");
+    return 0;
+}
